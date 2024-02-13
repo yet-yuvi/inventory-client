7711e025cfcb9ad2e2d4e8128608e300e76881f4
yet-yuvi <oscyanogen@gmail.com>
2024-02-14 05:16:56 +0600

    Commit-3 : css add and refactoring issue

--- src/components/navbar/NavMenuItem.jsx
+++ src/components/navbar/NavMenuItem.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export const NavMenuItem = ({title}) => {
     return(
-            <li className="navbar-menu-item">
+            <div className="navbar-menu-item">
                 <button>
                     {title}
                 </button>
-            </li> 
+            </div> 
     );
 }
