576ae7a390328495c343eb573840021556a84394
yet-yuvi <oscyanogen@gmail.com>
2024-02-14 05:16:56 +0600

    Commit-3 : css add and refactoring issue

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { NavMenuItem } from './NavMenuItem';
 
+import './Navbar.css';
+
 export function Navbar() {
+
+    const navItems = ['Home', 'Contact', 'Blog'];
+    const isAuthenticated = false;
+
     return(
       <div className="navbar">
         <div className="navbar-logo">
           <h1>Logo</h1>
         </div>
         <div className="navbar-menu">
-          <ul>
-            <NavMenuItem title="Learn" />
-            <NavMenuItem title="Learn" />
-            <NavMenuItem title="Learn" />
-            <NavMenuItem title="Learn" />
-          </ul>
+            {
+              navItems.map((navItem) => {
+                return(<NavMenuItem key={navItem} title={navItem} />);
+              })
+            }
+            {
+              isAuthenticated ? <button>Logout</button> : <button>Login</button>
+            }
         </div>
       </div>
     );
-  }
+  };
