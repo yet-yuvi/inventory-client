e7ea073159db9ed6f79bf538ae11cf2f0013926b
yet-yuvi <oscyanogen@gmail.com>
2024-02-17 03:21:17 +0600

    Commit-5 : Add a remove button to remove product from cart

--- src/components/products/productCard/ProductCard.jsx
+++ src/components/products/productCard/ProductCard.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
-import './ProductCard.css';
+// import React from 'react';
+// import './ProductCard.css';
 
-export const ProductCard = ({prod, addToCart}) => {
+// export const ProductCard = ({prod, addToCart}) => {
 
+//   const handleAddToCart = () => {
+//     addToCart(prod);
+//   }
+//   return (
+//     <div className='product-card'>
+//         <h1>{prod.name}</h1>
+//         <p>{`Tk ${prod.price}`}</p>
+//         <p>{`Quantity: ${prod.quantity}`}</p>
+//         <button onClick={handleAddToCart} className='add-cart-btn'>Add to Cart</button>
+//     </div>
+//   );
+// }
+
+// Remove product button added
+import React from "react";
+import "./ProductCard.css";
+
+export const ProductCard = ({
+  prod,
+  isAddedToCart,
+  addToCart,
+  removeFromCart,
+  cartItems,
+}) => {
   const handleAddToCart = () => {
     addToCart(prod);
-  }
+  };
+
+  const handleRemoveFromCart = () => {
+    if (cartItems === 0) {
+      alert("No product added");
+      return;
+    }
+    removeFromCart(prod.id);
+  };
+
   return (
-    <div className='product-card'>
-        <h1>{prod.name}</h1>
-        <p>{`Tk ${prod.price}`}</p>
-        <p>{`Quantity: ${prod.quantity}`}</p>
-        <button onClick={handleAddToCart} className='add-cart-btn'>Add to Cart</button>
+    <div className="product-card">
+      <h1>{prod.name}</h1>
+      <p>{`Tk ${prod.price}`}</p>
+      <p>{`Quantity: ${prod.quantity}`}</p>
+      <button onClick={handleAddToCart} className="add-cart-btn">
+        Add to Cart
+      </button>
+      {isAddedToCart && (
+        <button onClick={handleRemoveFromCart} className="remove-cart-btn">
+          Remove from Cart
+        </button>
+      )}
     </div>
   );
-}
+};
