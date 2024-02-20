5cb4431e83ba3dbf48e43cafef046d009ed10158
yet-yuvi <oscyanogen@gmail.com>
2024-02-20 07:27:21 +0600

    Commit-9 : Material UI Navbar and Icon added

---
+++ src/components/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { IconButton, Tooltip } from '../ui';
+import { CartIcon } from '../icons';
+
+export function GlobalCart() {
+  return (
+    <Tooltip title='Add to cart'>
+        <IconButton sx={{color:"white"}}>
+            <CartIcon />
+        </IconButton>
+    </Tooltip>
+  )
+}
