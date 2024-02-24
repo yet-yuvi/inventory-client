adba2cfe70430df2079c22d4ad4a7e18de9e5f00
yet-yuvi <oscyanogen@gmail.com>
2024-02-24 19:38:38 +0600

    Commit-12 : Refactoring some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Footer, Products } from "./components";
-import { CartContext, CartProvider } from "./contexts";
+import { CartProvider } from "./contexts";
 import "./App.css";
 
 function MainArea() {
