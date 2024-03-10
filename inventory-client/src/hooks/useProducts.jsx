230336bcf735208b555a49570b4ea884d6d53261
yet-yuvi <oscyanogen@gmail.com>
2024-03-11 04:52:28 +0600

    Commit-19 : Move useEffect logic to custom hook

--- inventory-client/src/hooks/useProducts.jsx
+++ inventory-client/src/hooks/useProducts.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
 import { ProductServices } from '../services';
 
 export function useProducts() {
-  const products = ProductServices.getProducts();
+  const [ products, setProducts ] = useState([]);
+
+  useEffect(() => {
+    ProductServices.getProducts()
+    .then((data) => setProducts(data))
+    .catch((e) => {
+      console.error(e);
+      alert('Failed to fetch products');
+    })
+  }, [])
 
   return {
     products,
