2bf7acc7ef6ab6d03fb29db0129ea1c09c8bf9e8
yet-yuvi <oscyanogen@gmail.com>
2024-03-12 20:53:19 +0600

    Commit-21 : Add product endpoint

--- inventory-server/src/routes/productRoutes.js
+++ inventory-server/src/routes/productRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ProductServices = require('../services/productServices');
+const { ProductServices } = require('../services');
 
 const productRouter = express.Router();
 
@@ -7,5 +7,14 @@ productRouter.get('/products', async (req, res) => {
     const products = await ProductServices.getProducts();
     res.json(products);
 });
+
+productRouter.post('/products', async (req, res) => {
+    try{
+        const product = await ProductServices.createProduct(req.body);
+        res.status(201).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
 
 module.exports = productRouter;
