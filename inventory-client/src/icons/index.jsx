593a89c35968a119a8151aba6379d6412b2bcda0
yet-yuvi <oscyanogen@gmail.com>
2024-03-15 02:06:11 +0600

    Commit-26 : Add loading state and Add custom component in dataGrid cell

--- inventory-client/src/icons/index.jsx
+++ inventory-client/src/icons/index.jsx
@@ -1,3 +1,15 @@
-import { Menu, Adb, ShoppingCartOutlined } from '@mui/icons-material';
+import {
+  Menu,
+  Adb,
+  ShoppingCartOutlined,
+  Edit,
+  Delete,
+} from "@mui/icons-material";
 
-export { Menu as MenuIcon, Adb as AdbIcon, ShoppingCartOutlined as CartIcon };
+export {
+  Menu as MenuIcon,
+  Adb as AdbIcon,
+  ShoppingCartOutlined as CartIcon,
+  Edit as EditIcon,
+  Delete as DeleteIcon,
+};
