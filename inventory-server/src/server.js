a8f78a77c804da2425f8f28e5d9ea644a837b344
yet-yuvi <oscyanogen@gmail.com>
2024-03-29 03:55:20 +0600

    Commit-39 : Add docker

--- inventory-server/src/server.js
+++ inventory-server/src/server.js
@@ -25,7 +25,7 @@ app.use('/api/users', userRouter);
 app.use('/api', productRouter);
 
 // Start the server
-const port = process.env.PORT || 6002;
+const port = process.env.PORT || 7000;
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
 })
